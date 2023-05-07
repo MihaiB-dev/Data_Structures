9e0e66f70e2d85a3e5a5e1644046ff7235fb4466
MihaiB-dev <mihaibivol.dev@gmail.com>
2023-05-08 00:34:14 +0300

    add case 4 and 5 to the remove part

--- tema2/source_files/remove.hpp
+++ tema2/source_files/remove.hpp
@@ -1,112 +1,85 @@
-// Node* RedBlackTree::remove_private(Node* node) {
-//     //find the succesor
-// 	if(node->right){//we are certain that we have a succesor.
-// 		Node* succesor = node->right;
-// 		while(succesor){
-// 			if(succesor->left){
-// 				succesor = succesor->left;
-// 			}else{break;}
-// 		}
-//         //change the the node.
-//         node->value = succesor->value;
-//         node->black = succesor->black;
-
-//         //delete the succesor.
-//         if(succesor->left) // daca mai are noduri in stanga
-//         {   
-//             succesor->left->parent = succesor->parent;
-//             succesor->parent->right = succesor->left;
-//             Node* copy = succesor->left;
-//             succesor = NULL;
-//             size --;
-//             return copy->parent;
-//         }else{
-//             if(succesor->right){ // daca mai are noduri in dreapta
-//                 succesor->parent->right = succesor->right;
-//                 succesor->right->parent = succesor->parent;
-//                 Node* copy = succesor->right;
-//                 succesor = NULL;
-//                 size --;
-//                 return copy->parent;
-//             }else{ // daca este leaf
-//                 if(succesor->isLeftChild){succesor->parent->left = NULL;}else{succesor->parent->right = NULL;}
-                
-//                 succesor = NULL;
-//                 size--;
-//                 return node;
-//             }
-            
-//         }
-        
-//     }
-//     if(node->left){//connect the left part to the parent of the node and disconnect the node.
-//         node->left->parent = node->parent;
-//         node->parent->right = node->left;
-//         Node* copy = node->left;
-//         node = NULL;
-//         size --;
-//         return copy->parent;
-//     }else{//it's a leaf , we can disconnect it directly, minus for size.
-//         if(node->isLeftChild){node->parent->left = NULL;} else {node->parent->right = NULL;}; 
-//         Node* parent = node->parent;
-//         node = NULL;
-//         size--;
-//         return parent; 
-//     }
-    
-
-// };
 void RedBlackTree::handleDoubleBlack(Node* node,bool isLeftChild)//first is parent of double black
-{   //node->doublBlack = false;
+{
     if(node==root){return;}
+    //CASE 3 DB's sibling is black, and bot of it's children are black :
+        //remove DB
+        //add black to it's parent
+        // if p is red it becomes black
+        // if p is black it becomes double black
+        //make sibling red
+
+    //CASE 4 if Db's sibling is red:
+    //swap colors of parent & it's sibling
+    //rotate parent in Db direction
+    //reply cases
+
+    //CASE 5
+    //db's sibing is black siblings chils who is far rom Db is black but
+    //near child to Db is red :
+    //swap colors of Db's sibling & siblings chil who is near to Db
+    //rotate sibling in oposite direction to apply case 6
     if(node->right && 
-        node->right->black == true && 
-        ((node->right->left == NULL && node->right->right == NULL)
+        node->right->black == true && isLeftChild == true) //CASE 3
+        {if((node->right->left == NULL && node->right->right == NULL)
         ||(node->right->left->black == true && node->right->right->black == true)
         ||(node->right->left == NULL && node->right->right->black == true)
         ||(node->right->right == NULL && node->right->left->black == true)
-        )
-        && isLeftChild == true)
-        {
+            )   
+        { //CASE 3
             if(node->black == true)
             {   
                  isLeftChild = node->left->isLeftChild; 
                  handleDoubleBlack(node->parent,isLeftChild);
+                 return;
             }else{node->black = true;}
             
             node->right->black = false;//sibling red
+        } else if(node->right->left->black == false && node->right->right && node->right->right->black == true)
+        {//CASE 5
+            node->right->left->black = true;
+            node->right->black = false;
+            rightRotate(node->right);
+        }
+
         }
         if(node->left && 
-        node->left->black == true &&
-        ((node->left->left == NULL && node->left->right == NULL)
+        node->left->black == true && isLeftChild == false)//CASE 3
+        if((node->left->left == NULL && node->left->right == NULL)
         ||(node->left->left->black == true && node->left->right->black == true)
         ||(node->left->left == NULL && node->left->right->black == true)
         ||(node->left->right == NULL && node->left->left->black == true)
-        )
-        && isLeftChild == false
-        )
-        {
+        ){//CASE 3
             if(node->black == true){
                 isLeftChild = node->right->isLeftChild; 
                 handleDoubleBlack(node->parent,isLeftChild);
+                return;
             }else{node->black = true;} 
             
             node->left->black = false; //sibling red
+        } else if(node->left->right->black = false && node->left->left && node->left->left->black == true )
+        {//CASE 5
+            node->left->right->black = true;
+            node->left->black = false;
+            leftRotate(node->left);
         }
     if(node->right && node->right->black == false && isLeftChild == true)
-    {
+    {//CASE 4
         node->right->black = true;
         node->black = false; 
         leftRotate(node);
-        //ADD
+
+        handleDoubleBlack(node->left, isLeftChild);
+        return;
     }
 
     if(node->left && node->left->black == false && isLeftChild == false)
-    {
+    {//CASE 4
         node->left->black = true;
         node->black = false;
         rightRotate(node);
-        //ADD
+
+        handleDoubleBlack(node->right, isLeftChild);
+        return;
     }
 }
 
@@ -126,12 +99,7 @@ void RedBlackTree::check_before_remove(Node* node){
         bool isLeftChild = node->isLeftChild;
         remove_private(node);
 
-        //if DB's sibling is black, and bot of it's children are black :
-        //remove DB
-        //add black to it's parent
-        // if p is red it becomes black
-        // if p is black it becomes double black
-        //make sibling red
+        
         handleDoubleBlack(parent_of_DB,isLeftChild);
         //-------------------------------------------------
 
