00c02cd89fa4ab54d494be1e0f24edbfa49a2cfa
MihaiB-dev <mihaibivol.dev@gmail.com>
2023-08-16 22:51:19 +0300

    translate into english and add readme

---
+++ InfoArena_Problems/rmq/readme.md
@@ -0,0 +1,34 @@
+# RMQ
+
+***Link: https://infoarena.ro/problema/rmq***
+A vector with N elements is given . Write a program that answers M questions like "What is the minimum element in the interval [ x , y ]?"
+
+### Input data
+On the first line of the rmq.in file, the numbers N and M are given . The next N lines will each contain a number representing the elements of the vector. The next M lines will each contain 2 numbers representing the x and y values ​​that define the questions.
+
+### Output data
+In the output file rmq.out there will be M lines, each containing a number, on line i is the answer to question i .
+
+### Restrictions
+- 1 ≤ N ≤ 100 000
+- 1 ≤ M ≤ 1 000 000
+- Numbers are the range [ 1,100 000 ]
+
+### Example
+rmq.in	  
+5 4  
+1  
+5  
+6  
+4  
+3  
+2 4  
+1 2  
+3 5  
+1 4  
+
+rmq.out  
+4  
+1    
+3  
+1  
--- InfoArena_Problems/rmq/rmq.cpp
+++ InfoArena_Problems/rmq/rmq.cpp
@@ -3,44 +3,44 @@
 using namespace std;
 const int maxel = 100001;
 const int log = 17; //log(max)
-int rmq[maxel][log]; //cream matricea pentru rmq
+int rmq[maxel][log]; //create a matrix for RMQ
 
-int query(int st, int dr){
-  int lungime = dr-st + 1;
+int query(int left, int right){
+  int length = right-left + 1;
   int k = 0;
-  while(1 << (k+1) <= lungime)k++;
-  //exemplu lungime = 17
-  //alege cea mai mare putere a lui 2 pana in 17
-  // 1, 2, 4, 8, 16
-  //=> k = 4 (2^4)
-  return min(rmq[st][k], rmq[dr - (1<<k) + 1][k]);
-  //nu este o problema daca avem "overlap"
+  while(1 << (k+1) <= length)k++; //other way to say log(length) but safer
+
+  return min(rmq[left][k], rmq[right - (1<<k) + 1][k]);
+  //it's not a problem if we have overlap
+  //Example for the formula:
   //0 1 2 3 4 5 6 7 8 9 10
-  //    |---------| avem st = 2 dr = 7 k = 4
-  //    |-----|     m[st][k]
-  //        |-----| m[dr - (1<<k) + 1][k]
+  //    |---------| we have left = 2 right = 7 k = 4 (return the min. in the range 2 to 7)
+  //    |-----|     m[left][k]  (min from 2 - 5 (k blocks)(starting from left index))
+  //        |-----| m[dr - (1<<k) + 1][k] (min from 4-7 (k blocks)(goes from the right side to left for k blocks))
 }
 
 int main(){
     ifstream fin("rmq.in");
     ofstream fout("rmq.out");
 
-    int N,M;// N = nr elemente, M = interogari
-    int st,dr;//[st,dr] = intervalul din care cautam minimul la interogari
+    int N,M;// N = number of elements, M = interogations
+    int st,dr;//[st,dr] = intervalul din care cautam minimul la interogations
     
     fin >> N >> M;
 
-    for(int i = 0; i < N; i ++)fin >> rmq[i][0]; //punem toate elementele in matrice
+    for(int i = 0; i < N; i ++)fin >> rmq[i][0]; //put all the elements in the matrix
 
     for(int j = 1; j < log; j ++)
       for(int i = 0; i <= N - (1<<j); i++)
-        rmq[i][j] = min(rmq[i][j-1], rmq[i+ (1<<(j-1))][j-1]);//atunci cand calcuam pentru bucket-uri mai mari, vedem minimul din cele mai mici.
+        //when we calculate for th bigger buckets, we will in fact look the the minimums of the smaller ones.
+        rmq[i][j] = min(rmq[i][j-1], rmq[i+ (1<<(j-1))][j-1]);
+
     //0 1 2 3 4 5 6 7 8 9 10 11 12
     //      |-----|
     //              |------|
     //      |--------------|
-    //cand calculam minimul pentru index(3) la log(8)= 3
-    //facem minimul din ultimele 2, ca sa nu pierdem timp
+    //Calculate the minimum for index(3) (i = 3) for log(8) = 3
+    //we make the minimum for the last 2, for less redundency
 
     for(int i = 0; i < M; i++)
     {
